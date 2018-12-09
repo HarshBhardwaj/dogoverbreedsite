9c940b150a3b31e9cce5a0c159106d96aa8a1085
Harshbhardwaj <harsh.s.bhardwaj@gmail.com>
2018-12-09 09:50:10 -0800

    Adding node modules to gitignore files

--- webProject/gulpfile.js
+++ webProject/gulpfile.js
@@ -1,5 +1,62 @@
-var gulp = require('gulp');
+var themename = 'dobcst';
 
-gulp.task('default', function()){
-  //place your code here
+var gulp = require('gulp');
+	//Prepare and optimize code etc
+	autoprefixer = require('autoprefixer'),
+	browserSync = require('browser-sync').create(),
+	postcss = require('gulp-postcss'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	jshint = require('gulp-jshint');
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+
+	//Only work with new or updated files
+	newer = require('gul-newer'),
+
+	//Name of working theme folder
+	root = '../' + themename + '/',
+	scss = root + 'sass/',
+	js = root + 'js/',
+		srcjs = root + 'srcjs/',
+	languages = root + 'languages/';
+
+//CSS via Sass Autoprofixer
+gulp.task('css', function(){
+	return gulp.src(scss + '{style.scss,rtl.scss}')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		outputStyle: 'expanded',
+		indentType: 'tab',
+		indentWidth: '1'
+	}).on('error', sass.logError))
+	.pipe(postcss([
+		autoprefixer('last 2 versions', '> 1%')]))
+	.pipe(sourcemaps.write(scss + 'maps'))
+	.pipe(gulp.dest(root));
 });
+
+//JavaScript
+gulp.task('javascript', function(){
+	return gulp.src([srcjs + '*.js'])
+	.pipe(jshint())
+	.pipe(jshint.reporter('fail'))
+	.pipe(concat('theme.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest(js));
+});
+
+//Watch everything
+gulp.task('watch', function(){
+	browserSync.init({
+		open: 'external',
+		proxy: 'http://localhost/wordpress/',
+		port: 3306
+	});
+	gulp.watch([root + '**/*.css', root + '**/*.scss'], ['css']);
+	gulp.watch(srcjs + '**/*.js', ['javascript']);
+		gulp.watch(root + '**/*').on('change', browserSync.reload);
+});
+
+//Default task (runs at initiation: gulp --verbose)
+gulp.task('default', ['watch']);
